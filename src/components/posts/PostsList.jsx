d7c35606b134b3fcb41106be9e1c39b614c09473
GokulKrishnaK771 <gokulkrishnak771@gmail.com>
2026-03-01 00:37:47 +0530

    BLog Post using redux

---
+++ src/components/posts/PostsList.jsx
@@ -0,0 +1,28 @@
+import {useSelector} from 'react-redux';
+import { selectAllPosts } from '../../features/posts/postsSlice';
+import PostAuthor from './PostAuthor';
+
+const PostsList = () => {
+
+    const posts = useSelector(selectAllPosts);
+
+    const renderedPosts = posts.map(post => (
+        <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0,100)}</p>
+            <p className='postsCredit'>
+              <PostAuthor userId = {post.userId}/>
+            </p>
+        </article>
+    ));
+
+
+  return (
+    <section>
+        <h2>Posts</h2>
+        {renderedPosts}
+    </section>
+  )
+}
+
+export default PostsList;
